246177d1df6d883f1644b4b31d64267aef74d309
Akhil Prasenan <akhil.prasenan@outlook.com>
2020-01-21 15:58:28 +0800

    added facebook authentication

--- config/passport.js
+++ config/passport.js
@@ -1,44 +1,77 @@
-const mongoose = require("mongoose");
-const User = mongoose.model("users");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("../config/keys.js");
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+const keys = require('../config/keys.js');
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+    done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => {
-    done(null, user);
-  });
+    User.findById(id).then(user => {
+        done(null, user);
+    });
 });
 
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.GOOGLE.clientID,
-      clientSecret: keys.GOOGLE.clientSecret,
-      callbackURL: "/api/users/google/callback",
-      proxy: true
-    },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ "google.id": profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({
-            google: {
-              id: profile.id,
-              token: accessToken
-            },
-            name: profile.displayName,
-            email: profile.emails[0].value
-          })
-            .save()
-            .then(user => done(null, user));
+    new GoogleStrategy(
+        {
+            clientID: keys.GOOGLE.clientID,
+            clientSecret: keys.GOOGLE.clientSecret,
+            callbackURL: '/api/users/google/callback',
+            proxy: true,
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ 'google.id': profile.id }).then(existingUser => {
+                if (existingUser) {
+                    done(null, existingUser);
+                } else {
+                    new User({
+                        google: {
+                            id: profile.id,
+                            token: accessToken,
+                        },
+                        name: profile.displayName,
+                        email: profile.emails[0].value,
+                    })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            });
         }
-      });
-    }
-  )
+    )
+);
+
+passport.use(
+    new FacebookStrategy(
+        {
+            clientID: keys.FACEBOOK.clientID,
+            clientSecret: keys.FACEBOOK.clientSecret,
+            callbackURL: '/api/users/facebook/callback',
+            profileFields: ['id', 'displayName', 'email'],
+            proxy: true,
+        },
+        function(accessToken, refreshToken, profile, done) {
+            User.findOne({ 'facebook.id': profile.id }).then(existingUser => {
+                console.log(profile);
+                if (existingUser) {
+                    done(null, existingUser);
+                } else {
+                    new User({
+                        facebook: {
+                            id: profile.id,
+                            token: accessToken,
+                        },
+                        name: profile.displayName,
+                        email: profile.emails[0].value,
+                    })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            });
+        }
+    )
 );
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,33 +1,46 @@
-const passport = require("passport");
-const express = require("express");
+const passport = require('passport');
+const express = require('express');
 
 const router = express.Router();
 
-const User = require("../../models/User");
+const User = require('../../models/User');
 
-router.get("/test", (req, res) => {
-  res.send("Auth Working properly");
+router.get('/test', (req, res) => {
+    res.send('Auth Working properly');
 });
 
 router.get(
-  "/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
+    '/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email'],
+    })
 );
 
-router.get("/google/callback", passport.authenticate("google"), (req, res) => {
-  res.redirect("/");
+router.get('/google/callback', passport.authenticate('google'), (req, res) => {
+    res.redirect('/');
 });
 
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/");
+router.get(
+    '/facebook',
+    passport.authenticate('facebook', { scope: ['email'] })
+);
+
+router.get(
+    '/facebook/callback',
+    passport.authenticate('facebook'),
+    (req, res) => {
+        res.redirect('/');
+    }
+);
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
 });
 
-router.get("/current_user", (req, res) => {
-  console.log(req);
-  res.send(req.user);
+router.get('/current_user', (req, res) => {
+    console.log(req);
+    res.send(req.user);
 });
 
 module.exports = router;
