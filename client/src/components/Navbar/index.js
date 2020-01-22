c361d6d4262bc4376da0e9470483f620af7973ab
Akhil Prasenan <akhil_prasenan@outlook.com>
2020-01-23 01:13:20 +0800

    Navbar mobile responsiveness updated

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,32 +6,58 @@ import { Link } from 'react-router-dom';
 import './style.scss';
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openMenu: false,
+    };
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState({
+      openMenu: !this.state.openMenu,
+    });
+  }
+
   render() {
     const { isAuthenticated, user } = this.props.auth;
-    console.log(user);
+
     const authLinks = (
-      <ul className="nav-links">
-        <li className="nav-item">
-          <Link className="nav-link" to="/create">
-            New Topic
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/all-topics">
-            All Topics
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/my-topics">
-            MY Topics
-          </Link>
-        </li>
-        <li className="nav-item">
-          <a className="btn--logout" href="/api/users/logout">
-            Logout
-          </a>
-        </li>
-      </ul>
+      <div className="menu-items">
+        <ul className={this.state.openMenu ? 'nav-links active' : 'nav-links'}>
+          <li className="nav-item">
+            <Link className="nav-link" to="/create">
+              New Topic
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/all-topics">
+              All Topics
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/my-topics">
+              MY Topics
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a className="btn--logout" href="/api/users/logout">
+              Logout
+            </a>
+          </li>
+        </ul>
+        <button
+          onClick={this.toggleMenu}
+          className={
+            this.state.openMenu ? 'menu-handle active' : 'menu-handle'
+          }>
+          <span className="top"></span>
+          <span className="middle"></span>
+          <span className="bottom"></span>
+        </button>
+      </div>
     );
 
     const guestLinks = (
--- client/src/components/TopicForm/index.js
+++ client/src/components/TopicForm/index.js
@@ -63,40 +63,42 @@ class TopicForm extends Component {
 
   render() {
     return (
-      <div className="topic-form">
-        {this.state.error && (
-          <div className="alert alert--error" role="alert">
-            {this.state.error}
-          </div>
-        )}
-        {this.state.postSuccess && (
-          <div className="alert alert--success" role="alert">
-            Posted Successfully
-          </div>
-        )}
-        <h1>Create New Topic</h1>
+      <div className="topic-form-wrapper">
+        <div className="topic-form">
+          {this.state.error && (
+            <div className="alert alert--error" role="alert">
+              {this.state.error}
+            </div>
+          )}
+          {this.state.postSuccess && (
+            <div className="alert alert--success" role="alert">
+              Posted Successfully
+            </div>
+          )}
+          <h1>Create New Topic</h1>
 
-        <form className="app-form" onSubmit={this.onSubmit}>
-          <label>Title</label>
-          <input
-            placeholder="Title of your topic"
-            onChange={this.onChange}
-            value={this.state.title}
-            type="text"
-            name="title"
-          />
-          <label>Description</label>
-          <textarea
-            placeholder="Description of your topic"
-            onChange={this.onChange}
-            value={this.state.description}
-            type="text"
-            name="description"
-          />
-          <button className="publish-btn btn--blue" type="submit">
-            Publish
-          </button>
-        </form>
+          <form className="app-form" onSubmit={this.onSubmit}>
+            <label>Title</label>
+            <input
+              placeholder="Title of your topic"
+              onChange={this.onChange}
+              value={this.state.title}
+              type="text"
+              name="title"
+            />
+            <label>Description</label>
+            <textarea
+              placeholder="Description of your topic"
+              onChange={this.onChange}
+              value={this.state.description}
+              type="text"
+              name="description"
+            />
+            <button className="publish-btn btn--blue" type="submit">
+              Publish
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
